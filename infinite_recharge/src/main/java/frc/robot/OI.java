347d1d1d0318b6d81a74ee4920b26542dd8e10ab
spunkygirl2002 <smallcrazyhall@gmail.com>
2020-01-11 16:01:49 -0500

    Added arcade drive to the IO

--- infinite_recharge/src/main/java/frc/robot/OI.java
+++ infinite_recharge/src/main/java/frc/robot/OI.java
@@ -1,10 +1,15 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 
 public class OI {
+
+    public static Joystick XboxDriver = new Joystick(RobotMap.xboxControllerDriver);
 
     public void init(){
 
     }
 }
+
+
     
